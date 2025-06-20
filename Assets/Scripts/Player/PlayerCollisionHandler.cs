cb0b505120aa9c81946b9d4e5518ed32da42ede1
Yagan <Yohan.Leykin@gmail.com>
2025-06-20 18:07:33 +0300

    added animations

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCoolDown = 1f;
     [SerializeField] float adjustChangeMoveSpeedAmount = -2f;
+    const string hitString = "Hit";
+    float coolDownTimer = 0f;
     LevelGenerator levelGenerator;
+    void Update()
+    {
+        coolDownTimer += Time.deltaTime;
+    }
     void Start()
     {
         levelGenerator = FindFirstObjectByType<LevelGenerator>();
     }
     void OnCollisionEnter(Collision collision)
     {
+        if (coolDownTimer < collisionCoolDown) return;
+        animator.SetTrigger(hitString);
         levelGenerator.ChangeChunkMoveSpeed(adjustChangeMoveSpeedAmount);
         Debug.Log(collision.gameObject.name);
     }
